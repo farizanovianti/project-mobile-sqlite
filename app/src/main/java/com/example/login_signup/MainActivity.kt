bd49e28d8d12b7fbd7ea4a858eafdfe53f41f866
farizanovianti <frznovianti11@gmail.com>
2025-02-19 13:53:27 +0700

    perbaikan logika

--- app/src/main/java/com/example/login_signup/MainActivity.kt
+++ app/src/main/java/com/example/login_signup/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.login_signup
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnExit: Button = findViewById(R.id.btnExit)
+
+        btnExit.setOnClickListener {
+            finishAffinity()
         }
     }
 }
